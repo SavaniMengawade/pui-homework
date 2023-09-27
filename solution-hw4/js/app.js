25c57be79982e2c7834adfff98e205070ded9e83
Savani Mengawade <68183338+SavaniMengawade@users.noreply.github.com>
2023-09-27 14:17:42 -0400

    HW4

--- solution-hw4/js/app.js
+++ solution-hw4/js/app.js
@@ -1,3 +1,14 @@
+class Roll {
+    constructor(rollType, rollGlazing, packSize, basePrice) {
+        this.type = rollType;
+        this.glazing =  rollGlazing;
+        this.size = packSize;
+        this.basePrice = basePrice;
+    }
+}
+
+
+//dictionary for populating choices
 let glazingChoice = {
     "keepOriginal": {name:"Keep Original", value:0},
     "sugarMilk": {name:"Sugar Milk", value:0},
@@ -13,7 +24,6 @@ let packChoice = {
 };
 
 
-
 let selectGlazing = document.getElementById('selectGlazing');
 selectGlazing.addEventListener('change', calcGlazingPrice);
 
@@ -24,7 +34,6 @@ selectPackSize.addEventListener('change', calcPackSize);
 let currentPrice = 2.49; 
 let glazingPrice = 0;
 let packPrice = 1;
-
 
 
 //populating options with glazing and pack choices
@@ -59,13 +68,55 @@ function calcPackSize() {
 
 }
 
+//extracting roll name
+const queryString = window.location.search;
+const params = new URLSearchParams(queryString);
+const rollType = params.get('roll');
+
+
+// console.log(rollType);
+
+const rollTitleElement = document.getElementById('rollTitle');
+rollTitleElement.innerText = rollType + " Cinnamon Roll";
+
+// console.log(rolls[rollType].imageFile);
+
+const rollImage = document.getElementById('rollProductImage');
+rollImage.src = '../assets/products/' + rolls[rollType].imageFile;
+
+let cart = [];   //empty array
+
+
+let displayPrice = document.getElementById('currentCartPrice');
+displayPrice.innerText = rolls[rollType].basePrice;
+
+
 
 // calculating final price
 function calcPrice(gp,pp){
-    currentPrice = 2.49;
+    currentPrice = rolls[rollType].basePrice;
     currentPrice = ((currentPrice+gp)*pp).toFixed(2);
     document.getElementById('currentCartPrice').innerHTML = currentPrice;
 }
+
+
+let addCart = document.getElementById('addCartButton');
+addCart.addEventListener('click', addToCartArray);
+
+function addToCartArray(){
+    let finalGlaze = selectGlazing.options[selectGlazing.selectedIndex].text;
+    let finalSize = selectPackSize.options[selectPackSize.selectedIndex].text;
+    let rollProduct = new Roll(rollType, finalGlaze, finalSize, rolls[rollType].basePrice);
+    cart.push(rollProduct);
+    console.log(cart);
+}
+
+
+//Reference to select the selected roll : https://stackoverflow.com/questions/1085801/get-selected-value-in-dropdown-list-using-javascript
+
+
+
+
 
 
 
